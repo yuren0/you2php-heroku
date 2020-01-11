701a4044abd43b8d4a80f7c20b7c0422a8e13872
yuren0 <45882709+yuren0@users.noreply.github.com>
2020-01-11 15:53:07 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyAGV-znlAx21zsD4aMLMk4Re5Iy5QW2DhI');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'Hello World!');
-define('TITLENAME', 'Hello World!');
+define('SITE_NAME', '最爱咱家王大爷');
+define('TITLENAME', '最爱咱家王大爷');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
